952d396b697ee408da21b5d634a35e41b43038d9
randolf@jenkins-randolf <randolf.kastenmaier@gmail.com>
2013-08-15 10:25:04 +0200

    Aligned to latest update and added lgdrm checks @pengus77 Change-Id: Ia957ccf9fba5b8140359bb2fe23f7ccc93199e09

--- check_sdcard.sh
+++ check_sdcard.sh
@@ -5,3 +5,9 @@ if [ "$CHECK" == "" ]
 then
 	/system/bin/newfs_msdos -F 32 /dev/block/mmcblk0p11
 fi
+
+CHECK=$(/system/xbin/blkid /dev/block/mmcblk0p7 | grep ext3)
+if [ "$CHECK" == "" ]
+then
+	/sbin/dd if=/tmp/lgdrm.img of=/dev/block/mmcblk0p7
+fi
