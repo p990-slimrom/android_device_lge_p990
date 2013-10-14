0f2443b6b7ea57ea04b66c8cdaccd32140b10f2e
Randolf Kastenmaier <randolf.kastenmaier@gmail.com>
2013-10-14 21:29:35 +0200

    camera wrapper: the forgotten stuff
    Change-Id: Iedfb0340ca639e9e97df864816f266c6285fefac

--- p990.mk
+++ p990.mk
@@ -81,6 +81,11 @@ PRODUCT_PACKAGES += \
 # Enable Torch
 PRODUCT_PACKAGES += Torch
 
+# Camera Wrapper
+PRODUCT_PACKAGES += \
+    camerawrapper \
+    camera.tegra
+
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp,adb
