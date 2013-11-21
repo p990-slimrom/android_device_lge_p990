8216f545755013148612da0edfa1c71202c4990f
hjunii <hjunii@gmail.com>
2013-11-21 21:59:00 +0900

    squashed SU660 Patches
    Add the SU660 patches by @hjunii
    Signed-off-by: tonyp <tonyptonyp@gmail.com>
    
    SU660 Patch
    Change-Id: I07d36fae21e1c3ff557d9886a109888682721adf
    Fix camera
    Change-Id: Idc66094d0f0cf96c0a96c1de83319a1bd8da3f9c
    Fix handling if applying patch is failed
    Change-Id: Iecf6e9e864e53d62fc75bbd5496602aa15d7904f
    Kernel Update - p990 relates update
    Kernel Update - enable KSM
    Change-Id: Ib759bb20171bb8cbd41cd1934070a1a9955c078e
    Updated mHead base screenshot patch
    Change-Id: I2d7f3196f96035df0c74e03a13ec0d223a0cfb9a
    Fix bluetooth - update kernel and device tree
    Change-Id: I5177b8a273e2cf0e5913bfc3e8266f72cf52219b
    Update kernel
    Change-Id: I49d9d5af3315ba8e60015241137e32724bd9edb7
    Update patches
    Change-Id: I9c283c9672f86be85fd94e51c9c03d22d981a3fd
    Update Kernel - change some kernel config
    Change-Id: Ia3f2db66f62c09235e8210aa98bcb0a93b268d30
    Add patch - external/skia
    Add patch for external/chromium_org
    Change-Id: Idd0c144c5648c5bf06c3cc8d30012b17018bb755
    Fix patch logic
    Change-Id: I55a226e6972c63e5965434135c7fa745635b31da
    Fix build error with latest cm11
    Change-Id: Ifa3857abcdd3cc364021d01a5baf2e3ebeb29df2
    Kernel update & remove unnecessary patches
    Change-Id: I86723954ef0d0ae796b67eacf5bd05fb3a703d46
    Restore EGL patch and add start audio patch
    Change-Id: I04d4b3d3b54832396f6d40990ca32752abb1a81f
    Restore wifi patch for compatibility
    Change-Id: Ie945c2435e4b782d224afe885516e9b58729b691
    Remove unnecessary patch
    Change-Id: I7315f8dd53e6b4f762ffa532d4b0eebb69c40c48

---
+++ vendorsetup.sh
@@ -0,0 +1,81 @@
+#
+# Copyright 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+add_lunch_combo slim_p990-userdebug
+add_lunch_combo cm_p990-userdebug
+
+echo ""
+echo "Applying patches for P990"
+echo ""
+
+echo "Apply patch to frameworks/base"
+echo -n "Apply patch 0001-framework-base-patch.patch"
+(cd frameworks/base; git am ../../device/lge/p990/patches/0001-framework-base-patch.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+       echo "     [DONE]"
+else
+       (cd frameworks/base; git am --abort)
+       echo "     [FAIL]"
+fi
+
+echo "Apply patch to frameworks/av"
+echo -n "Apply patch 0001-ifdef-for-ICS-Audio-Blob-compatibility.patch"
+(cd frameworks/av; git am ../../device/lge/p990/patches/0001-ifdef-for-ICS-Audio-Blob-compatibility.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/av; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo -n "Apply patch 0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch"
+(cd frameworks/av; git am ../../device/lge/p990/patches/0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd frameworks/av; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo "Apply patch to external/skia"
+echo -n "Apply patch 0001-external-skia-patch.patch"
+(cd external/skia; git am ../../device/lge/p990/patches/0001-external-skia-patch.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd external/skia; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo "Apply patch to external/chromium_org"
+echo -n "Apply patch 0001-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch"
+(cd external/chromium_org; git am ../../device/lge/p990/patches/0001-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd external/chromium_org; git am --abort)
+	echo "     [FAIL]"
+fi
+
+echo "Apply patch to hardware/libhardware"
+echo -n "Apply patch 0001-Star-audio-patch.patch"
+(cd hardware/libhardware; git am ../../device/lge/p990/patches/0001-Star-audio-patch.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+	echo "     [DONE]"
+else
+	(cd hardware/libhardware; git am --abort)
+	echo "     [FAIL]"
+fi
