4909ea30013917a95e409e2a8c7f875ed9e5b02a
tonyp <tonyptonyp@gmail.com>
2013-12-23 20:28:55 +0100

    include kernel kk 31

--- p990.mk
+++ p990.mk
@@ -139,8 +139,6 @@ LOCAL_KERNEL := device/lge/p990/kernel/zImage
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel \
     $(LOCAL_PATH)/kernel/bthid.ko:system/lib/modules/bthid.ko \
-    $(LOCAL_PATH)/kernel/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-    $(LOCAL_PATH)/kernel/wireless.ko:system/lib/modules/wireless.ko \
     $(LOCAL_PATH)/kernel/zram.ko:system/lib/modules/zram.ko
 
 PRODUCT_COPY_FILES += \
