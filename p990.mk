fd0bfab648681a31ab5aa1e94319ac82d194fee1
Randolf Kastenmaier <randolf.kastenmaier@gmail.com>
2013-12-24 14:02:33 +0100

    kernel: modify mk file
    Change-Id: I6c7aba2acb3fe32e55aeb65c389cce6c32f4ae56

--- p990.mk
+++ p990.mk
@@ -134,16 +134,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel \
     $(LOCAL_PATH)/kernel/bthid.ko:system/lib/modules/bthid.ko \
     $(LOCAL_PATH)/kernel/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-    $(LOCAL_PATH)/kernel/wireless.ko:system/lib/modules/wireless.ko \
     $(LOCAL_PATH)/kernel/zram.ko:system/lib/modules/zram.ko
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/kernel/kowalski/power.tegra.so:system/lib/hw/power.tegra.so \
     $(LOCAL_PATH)/kernel/kowalski/KowalskiManager.apk:system/app/KowalskiManager.apk \
     $(LOCAL_PATH)/kernel/kowalski/95kowalski:system/etc/init.d/95kowalski \
-    $(LOCAL_PATH)/kernel/kowalski/alsa_amixer:system/xbin/alsa_amixer \
-    $(LOCAL_PATH)/kernel/kowalski/iwconfig:system/xbin/iwconfig \
-    $(LOCAL_PATH)/kernel/kowalski/iwlist:system/xbin/iwlist
+    $(LOCAL_PATH)/kernel/kowalski/alsa_amixer:system/xbin/alsa_amixer
 
 PRODUCT_PACKAGES += \
     lgcpversion
