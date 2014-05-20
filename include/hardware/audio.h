1dd0319199d185c6e7fc10179176ac198de39ee2
Firtecy <admin@firtecy.de>
2014-05-20 16:25:37 +0200

    Audio: Fix build error, because of missing audio format
    Change-Id: I87ce7ccbb9bcad87ada6c1414d698c6f5edf12fd

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -170,6 +170,7 @@ __BEGIN_DECLS
 #define AUDIO_OFFLOAD_CODEC_DOWN_SAMPLING  "music_offload_down_sampling"
 #define AUDIO_OFFLOAD_CODEC_DELAY_SAMPLES  "delay_samples"
 #define AUDIO_OFFLOAD_CODEC_PADDING_SAMPLES  "padding_samples"
+#define AUDIO_OFFLOAD_CODEC_FORMAT  "music_offload_codec_format"
 
 /**************************************/
 
