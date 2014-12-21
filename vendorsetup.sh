16478528eff5bba1542701db4e8923ebeede9015
Winston <wulsic@hotmail.com>
2014-12-21 21:15:47 +0100

    Update vendorsetup.sh
    Some patches got removed and some got added
    Conflicts:
    	vendorsetup.sh
    
    Change-Id: I5418af5b24e012008ee304f99d26134311dc0841

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,43 +22,42 @@ echo "Applying patches for P990"
 echo ""
 
 DIR=$(pwd)
+echo "Apply patches related to SlimRom"
 echo -n "revert dpi changes"
-cd $DIR/vendor/slim/; git fetch https://github.com/p990-slimrom/vendor_slim.git && git cherry-pick dec1c9af22385a73aba37835b4eed38f057e5d6e
-
-
-#echo -n "apply JustArchi's ArchiDroid Optimizations V3"
-#cd $DIR/build/; git fetch https://github.com/SlimKatXperia/android_build.git && git cherry-pick 6b734ccc7aa260b113e07bc84577e4414362f9e3
-
-#echo -n "Apply gcc4.8 changes"
-#cd $DIR/build/; git fetch https://github.com/p990-slimrom/android_build.git && git cherry-pick 99ef9408ab4b04b440cc9aeed9d37f849c7b9723
-
-#removed for the moment
-#echo -n "remove the selinux packages"
-#cd $DIR/build/; git fetch https://github.com/p990-slimrom/android_build.git && git cherry-pick 0564f2b5031c60f146aaf7e500835dea4f4d4e92
-
-#echo -n "Apply extFAT patch"
-#cd $DIR/external/fuse/; git fetch https://github.com/JustArchi/android_external_fuse.git && git cherry-pick 78ebbc4404de260862dca5f0454bffccee650e0d
-
-#echo -n "Apply patch to external/valgrind"
-#cd $DIR/external/valgrind/; git fetch https://github.com/XperiaSTE/android_external_valgrind.git && git cherry-pick 679a4e571ef77f47892a785e852d8219c1e6807a
-
-#echo -n "Apply blueZ changes to bionic"
-#cd $DIR/bionic/; git fetch https://github.com/SlimKatXperia/android_bionic.git && git cherry-pick ce07db5aa9855e1910ea0436ff1d7fbd7ffd74ba
-
-echo -n "remove SliomOTA"
-cd $DIR/packages/apps/SlimCenter/; git fetch https://github.com/p990-slimrom/packages_apps_SlimCenter.git && git cherry-pick 1fdec6e946901d7d0a0f5e5f147fca6dc81ee9da
-cd $DIR/packages/apps/SlimCenter/; git fetch https://github.com/p990-slimrom/packages_apps_SlimCenter.git && git cherry-pick 4109dd21a6c7386f017417f982722a123f024364
-
-cd $DIR/
-
-
-echo "Apply patch to frameworks/base"
-echo -n "Apply patch 0001-framework-base-patch.patch"
-(cd frameworks/base; git am ../../device/lge/p990/patches/0001-framework-base-patch.patch) > /dev/null 2>&1
+cd vendor/slim/; git fetch https://github.com/p990-slimrom/vendor_slim.git && git cherry-pick dec1c9af22385a73aba37835b4eed38f057e5d6e > /dev/null 2>&1
 if [ $? == 0 ]; then
        echo "     [DONE]"
 else
-       (cd frameworks/base; git am --abort)
+       (cd vendor/slim/; git cherry-pick --abort)
+       echo "     [FAIL]"
+fi
+
+
+echo -n "remove SliomOTA"
+cd packages/apps/SlimCenter/; git fetch https://github.com/p990-slimrom/packages_apps_SlimCenter.git && git cherry-pick 1fdec6e946901d7d0a0f5e5f147fca6dc81ee9da > /dev/null 2>&1
+if [ $? == 0 ]; then
+       echo "     [DONE]"
+else
+       (cd packages/apps/SlimCenter/; git cherry-pick --abort)
+       echo "     [FAIL]"
+fi
+
+cd packages/apps/SlimCenter/; git fetch https://github.com/p990-slimrom/packages_apps_SlimCenter.git && git cherry-pick 4109dd21a6c7386f017417f982722a123f024364 > /dev/null 2>&1
+if [ $? == 0 ]; then
+       echo "     [DONE]"
+else
+       (cd packages/apps/SlimCenter/; git cherry-pick --abort)
+       echo "     [FAIL]"
+fi
+
+
+echo "Apply patch to frameworks/native"
+echo -n "Apply patch 0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch"
+(cd frameworks/native; git am ../../device/lge/p990/patches/0002-DisplayDevice-Backwards-compatibility-with-old-EGL.patch) > /dev/null 2>&1
+if [ $? == 0 ]; then
+       echo "     [DONE]"
+else
+       (cd frameworks/native; git am --abort)
        echo "     [FAIL]"
 fi
 
@@ -72,31 +71,12 @@ else
        echo "     [FAIL]"
 fi
 
-echo "Apply patch to frameworks/av"
-echo -n "Apply patch 0001-ifdef-for-ICS-Audio-Blob-compatibility.patch"
-(cd frameworks/av; git am ../../device/lge/p990/patches/0001-ifdef-for-ICS-Audio-Blob-compatibility.patch) > /dev/null 2>&1
+echo "Apply patch to external/ffmpeg"
+echo -n "Apply patch 0001-FFMPEG-Backwards-compatibility-with-non-neon-devices.patch"
+(cd external/ffmpeg; git am ../../device/lge/p990/patches/0001-FFMPEG-Backwards-compatibility-with-non-neon-devices.patch) > /dev/null 2>&1
 if [ $? == 0 ]; then
 	echo "     [DONE]"
 else
-	(cd frameworks/av; git am --abort)
-	echo "     [FAIL]"
-fi
-
-echo -n "Apply patch 0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch"
-(cd frameworks/av; git am ../../device/lge/p990/patches/0002-Add-missing-functions-and-signatures-for-older-OMX-v.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-	echo "     [DONE]"
-else
-	(cd frameworks/av; git am --abort)
-	echo "     [FAIL]"
-fi
-
-echo "Apply patch to external/chromium_org"
-echo -n "Apply patch 0001-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch"
-(cd external/chromium_org; git am ../../device/lge/p990/patches/0001-Work-around-broken-GL_TEXTURE_BINDING_EXTERNAL_OES-q.patch) > /dev/null 2>&1
-if [ $? == 0 ]; then
-	echo "     [DONE]"
-else
-	(cd external/chromium_org; git am --abort)
+	(cd external/ffmpeg; git am --abort)
 	echo "     [FAIL]"
 fi
