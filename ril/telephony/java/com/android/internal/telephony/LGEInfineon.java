44f3c8573b284354cdcece66d29ddb1bde25dc75
tonyp <tonyptonyp@gmail.com>
2015-02-06 03:22:19 +0100

    Fix automatic time on Basebands v28e or newer
    by setting RIL_UNSOL_NITZ_TIME_RECEIVED to true.
    This will fix automatic time on Basebands v28e or newer by default.
    
    Users of older Baseband versions will have to add the following line to
    their build.prop:
    telephony.test.ignore.nitz=false
    
    This commit is based on the assumption that the newer Basebands are used
    by the majority nowadays.

--- ril/telephony/java/com/android/internal/telephony/LGEInfineon.java
+++ ril/telephony/java/com/android/internal/telephony/LGEInfineon.java
@@ -267,25 +267,9 @@ public class LGEInfineon extends RIL implements CommandsInterface {
                 result[0] = ret;
                 result[1] = Long.valueOf(nitzReceiveTime);
 
-                // RIL_UNSOL_NITZ_TIME_RECEIVED handling
-                boolean ignoreNitz = false;
-                if (SystemProperties.getBoolean(TelephonyProperties.PROPERTY_IGNORE_NITZ, false)) {
-                    ignoreNitz = true;
-                } else {
-                    if ("LGSU660AT".equalsIgnoreCase(basebandSplit[0])) {
-                       ignoreNitz = true;
-                    } else {
-                    	// Detect V28e or newer BBs like
-                        // LGP990AT-00-V30a-EUR-XXX-NOV-30-2012+0
-                        if ("LGP990AT".equalsIgnoreCase(basebandSplit[0])
-                        		&& (basebandSplit.length > 2)
-                               	 	&& (basebandSplit[2].length() == 4)
-                                	&& (basebandSplit[2].toLowerCase().startsWith("v"))
-                                	&& (basebandSplit[2].compareToIgnoreCase("V28e") >= 0)) {
-                        	ignoreNitz = true;
-                        }
-                    }
-                }
+                // default RIL_UNSOL_NITZ_TIME_RECEIVED to true
+                boolean ignoreNitz = SystemProperties.getBoolean(TelephonyProperties.PROPERTY_IGNORE_NITZ, true);
+
                 if (ignoreNitz) {
                     if (RILJ_LOGD) riljLog("ignoring UNSOL_NITZ_TIME_RECEIVED");
                 } else {
