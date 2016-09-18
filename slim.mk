d6bc827f5b014429dd5c914e72435b653c4a3501
Randolf Kastenmaier <randolf.kastenmaier@gmail.com>
2016-09-18 20:56:20 +0200

    add slim and add automatic slim upstream merege
    Change-Id: I78040e09cbc55c5ae93ab3b21b8cb46bf4f48bc4

---
+++ slim.mk
@@ -0,0 +1,26 @@
+# TW
+TW_THEME := portrait_hdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/p990/p990.mk)
+
+# TW
+TW_THEME := portrait_hdpi
+
+PRODUCT_NAME := slim_p990
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := Optimus2X
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := p990
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=star_open_eu BUILD_ID=IMM76L BUILD_FINGERPRINT=lge/star_open_eu/star:4.0.4/IMM76L/LG-P990-P99030a.1211302332:user/release-keys PRIVATE_BUILD_DESC="star_open_eu-user 4.0.4 IMM76L LG-P990-P99030a.1211302332 release-keys"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,6 +14,93 @@
 # limitations under the License.
 #
 
-add_lunch_combo cm_p990-userdebug
+add_lunch_combo slim_p990-userdebug
+
+echo ""
+echo "Applying patches for P990"
+echo ""
+
+DIR=$(pwd)
+
+mkdir -p out/target/product/p990/obj/KERNEL_OBJ/usr
+
+echo -n "build: merge upstream"
+cd $DIR/build/;
+git remote add slim https://github.com/SlimRoms/android_build.git
+git fetch slim
+git pull slim lp5.1
+git remote add p990 git@github.com:p990-slimrom/frameworks_av.git
+git fetch p990
+#git commit -m "merge branch"
+#git push p990 HEAD:lp5.1
+echo -n ""
 
 
+echo -n "frameworks_av: merge upstream"
+cd $DIR/frameworks/av/;
+git remote add slim https://github.com/SlimRoms/frameworks_av.git
+git fetch slim
+git pull slim lp5.1
+git remote add p990 git@github.com:p990-slimrom/frameworks_av.git
+git fetch p990
+#git commit -m "merge branch"
+#git push p990 HEAD:lp5.1
+echo -n ""
+
+echo -n "frameworks_native: merge upstream"
+cd $DIR/frameworks/native/;
+git remote add slim https://github.com/SlimRoms/frameworks_native.git
+git fetch slim
+git pull slim lp5.1
+git remote add p990 git@github.com:p990-slimrom/frameworks_native.git
+git fetch p990
+#git commit -m "merge branch"
+#git push p990 HEAD:lp5.1
+echo -n ""
+
+echo -n "frameworks_wilhelm: merge upstream"
+cd $DIR/frameworks/wilhelm/;
+git remote add slim https://github.com/SlimRoms/frameworks_wilhelm.git
+git fetch slim
+git pull slim lp5.1
+git remote add p990 git@github.com:p990-slimrom/frameworks_wilhelm.git
+git fetch p990
+#git commit -m "merge branch"
+#git push p990 HEAD:lp5.1
+echo -n ""
+
+echo -n "hardware_libhardware: merge upstream"
+cd $DIR/hardware/libhardware/;
+git remote add slim https://github.com/SlimRoms/hardware_libhardware.git
+git fetch slim
+git pull slim lp5.1
+git remote add p990 git@github.com:p990-slimrom/hardware_libhardware.git
+git fetch p990
+#git commit -m "merge branch"
+#git push p990 HEAD:lp5.1
+echo -n ""
+
+echo -n "hardware_libhardware_legacy: merge upstream"
+cd $DIR/hardware/libhardware_legacy/;
+git remote add slim https://github.com/SlimRoms/hardware_libhardware_legacy.git
+git fetch slim
+git pull slim lp5.1
+git remote add p990 git@github.com:p990-slimrom/hardware_libhardware_legacy.git
+git fetch p990
+#git commit -m "merge branch"
+#git push p990 HEAD:lp5.1
+echo -n ""
+
+echo -n "external ffmpeg: merge upstream"
+cd $DIR/external/ffmpeg/;
+git remote add slim https://github.com/SlimRoms/android_external_ffmpeg
+git fetch slim
+git pull slim lp5.1
+git remote add p990 git@github.com:p990-slimrom/android_external_ffmpeg.git
+git fetch p990
+#git commit -m "merge branch"
+#git push p990 HEAD:lp5.1
+echo -n ""
+
+
+cd $DIR/
